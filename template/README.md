9c88c611a195cf53e96565bb08fc7ddedaca4406
Jaskaran Singh <snak3codes@outlook.com>
2020-04-13 14:03:24 -0400

    Finished template

---
+++ template/README.md
@@ -0,0 +1,1 @@
+This is a custom CRA template created by Snak3Codes
---
+++ template/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      Snak3codes awesome CRA Template
+    </div>
+  );
+}
+
+export default App;
