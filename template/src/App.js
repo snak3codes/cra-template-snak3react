acb9f83422f81c2f512b80b91935c11cc1ea7db4
Jaskaran Singh <snak3codes@outlook.com>
2020-04-13 17:55:40 -0400

    Added home screen

--- template/src/App.js
+++ template/src/App.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
+import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      Snak3codes awesome CRA Template
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>
+          Welcome to <span className="framework">React</span>{' '}
+        </h1>
+        <img src="logo192.png" alt="" height="80px" />
+        <p>
+          You are using the{' '}
+          <strong>
+            <em className="framework">snak3react</em>
+          </strong>{' '}
+          CRA Template
+        </p>
+      </div>
+    );
+  }
 }
 
 export default App;
